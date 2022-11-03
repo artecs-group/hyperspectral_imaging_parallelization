e7aa542757b389afade91521097a9d3094d9949e
FRYoussef <f.r.youssef@hotmail.com>
2022-11-03 15:52:32 +0100

    (WIP) Main file refactor

---
+++ src/common.hpp
@@ -0,0 +1,7 @@
+#ifndef _COMMON_
+#define _COMMON_
+
+#define MAXLINE 200
+#define MAXCAD 200
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,258 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+#include "common.hpp"
+
+/*
+ * Author: Jorge Sevilla Cedillo
+ * Centre: Universidad de Extremadura 
+ */
+void cleanString(char* str, char* out) {
+    int i,j;
+    for( i = j = 0; str[i] != 0; i++) {
+        if(isalnum(str[i]) || str[i] == '{' || str[i]== '.' || str[i] == ',') {
+            out[j] = str[i];
+            j++;
+        }
+    }
+    for( i = j; out[i] != 0; i++)
+        out[j] = 0;
+}
+
+
+/*
+ * Author: Jorge Sevilla Cedillo & Youssef El Faqir El Rhazoui
+ */
+int readHeader1(char* filename, int* lines, int* samples, int* bands, int* dataType,
+		char* interleave, int* byteOrder, char* waveUnit)
+{
+    FILE* fp;
+    char line[MAXLINE] = "";
+    char value [MAXLINE] = "";
+
+    if ((fp=fopen(filename, "rt")) != NULL)
+    {
+        fseek(fp, 0L, SEEK_SET);
+        while(fgets(line, MAXLINE, fp) != NULL) {
+            //Samples
+            if(strstr(line, "samples") != NULL && samples != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                *samples = atoi(value);
+            }
+
+            //Lines
+            else if(strstr(line, "lines") != NULL && lines != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                *lines = atoi(value);
+            }
+
+            //Bands
+            else if(strstr(line, "bands") != NULL && bands != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                *bands = atoi(value);
+            }
+
+            //Interleave
+            else if(strstr(line, "interleave") != NULL && interleave != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                strcpy(interleave,value);
+            }
+
+            //Data Type
+            else if(strstr(line, "data type") != NULL && dataType != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                *dataType = atoi(value);
+            }
+
+            //Byte Order
+            else if(strstr(line, "byte order") != NULL && byteOrder != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                *byteOrder = atoi(value);
+            }
+
+            //Wavelength Unit
+            else if(strstr(line, "wavelength unit") != NULL && waveUnit != NULL)
+            {
+                cleanString(strstr(line, "="),value);
+                strcpy(waveUnit,value);
+            }
+        }
+        fclose(fp);
+        return 0;
+    }
+    else
+    	return -2; //No file found
+}
+
+
+int readHeader2(char* filename, double* wavelength) {
+    FILE* fp;
+    char line[MAXLINE] = "";
+    char value [MAXLINE] = "";
+
+    if ((fp=fopen(filename,"rt")) != NULL)
+    {
+        fseek(fp,0L,SEEK_SET);
+        while(fgets(line, MAXLINE, fp) != NULL)
+        {
+            //Wavelength
+            if(strstr(line, "wavelength =") != NULL && wavelength != NULL) {
+                char strAll[100000] = "";
+                char* pch;
+                int cont = 0;
+
+                do {
+                    fgets(line, MAXLINE, fp);
+                    cleanString(line,value);
+                    strcat(strAll,value);
+                } while (strstr(line, "}") == NULL);
+
+                pch = strtok(strAll,",");
+
+                while (pch != NULL) {
+                    wavelength[cont] = atof(pch);
+                    pch = strtok(NULL, ",");
+                    cont++;
+                }
+            }
+		}
+		fclose(fp);
+		return 0;
+	}
+	else
+		return -2; //No file found
+}
+
+
+template <typename T>
+void convertToFloat(long int lines_samples, int bands, FILE *fp, float* type_float){
+    T* typeValue = new T[lines_samples*bands*sizeof(T)];
+    fread(typeValue, 1, (sizeof(T) * lines_samples * bands), fp);
+    for(int i = 0; i < lines_samples * bands; i++)
+        type_float[i] = (float) typeValue[i];
+    delete[] typeValue;
+}
+
+
+/*
+ * Author: Jorge Sevilla Cedillo & Youssef El Faqir El Rhazoui
+ * Centre: Universidad de Extremadura
+ * */
+int loadImage(char* filename, double* image, int lines, int samples, int bands, int dataType, char* interleave) {
+    FILE *fp;
+    float *type_float;
+    int op;
+    long int lines_samples = lines*samples;
+    short int* tv;
+
+    if ((fp=fopen(filename,"rb"))!=NULL) {
+
+        fseek(fp, 0L, SEEK_SET);
+        type_float = new float[lines_samples*bands*sizeof(float)];
+        
+        switch(dataType) {
+            case 2:
+                convertToFloat<short int>(lines_samples, bands, fp, type_float);
+                break;
+            case 4:
+                fread(type_float, 1, (sizeof(float)*lines_samples*bands), fp);
+                break;
+            case 5:
+                convertToFloat<double>(lines_samples, bands, fp, type_float);
+                break;
+            case 12:
+                convertToFloat<unsigned int>(lines_samples, bands, fp, type_float);
+                break;
+            default:
+                break;
+        }
+        fclose(fp);
+
+        if(interleave == NULL)
+            op = 0;
+        else
+        {
+            if(strcmp(interleave, "bsq") == 0) op = 0;
+            if(strcmp(interleave, "bip") == 0) op = 1;
+            if(strcmp(interleave, "bil") == 0) op = 2;
+        }
+
+        switch(op) {
+            case 0:
+                for(int i = 0; i < lines*samples*bands; i++)
+                    image[i] = type_float[i];
+                break;
+
+            case 1:
+                for(int i = 0; i < bands; i++)
+                    for(int j = 0; j < lines*samples; j++)
+                        image[i*lines*samples + j] = type_float[j*bands + i];
+                break;
+
+            case 2:
+                for(int i = 0; i < lines; i++)
+                    for(int j = 0; j < bands; j++)
+                        for(int k = 0; k < samples; k++)
+                            image[j*lines*samples + (i*samples+k)] = type_float[k+samples*(i*bands+j)];
+                break;
+        }
+        delete[] type_float;
+        return 0;
+    }
+    return -2;
+}
+
+
+int main(int argc, char* argv[]) {
+	if(argc != 3) {
+        std::cout << "Parameters are not correct." << std::endl
+                  << "./main <Image Filename> <Approximation value>" << std::endl;
+		exit(-1);
+	}
+
+	// Read image
+	char cad[MAXCAD];
+	int lines{0}, samples{0}, bands{0}, dataType{0}, byteOrder{0};
+	char* interleave = new char[MAXCAD * sizeof(char)];
+	char* waveUnit = new char[MAXCAD * sizeof(char)];
+
+    // Read header first parameters
+	strcpy(cad, argv[1]);
+	strcat(cad, ".hdr");
+	int error = readHeader1(cad, &lines, &samples, &bands, &dataType, interleave, &byteOrder, waveUnit);
+	if(error != 0) {
+        std::cout << "Error reading header file: " << cad << std::endl; 
+		exit(-1);
+	}
+
+    // Read header wavelenght, which requires bands from previous read
+    double* wavelength = new double[bands * sizeof(double)];
+    error = readHeader2(cad, wavelength);
+	if(error != 0) {
+        std::cout << "Error reading wavelength from header file: " << cad << std::endl; 
+		exit(-1);
+	}
+
+	double *image = new double[lines*samples*bands*sizeof(double)];
+	error = loadImage(argv[1], image, lines, samples, bands, dataType, interleave);
+	if(error != 0) {
+        std::cout << "Error reading image file: " << argv[1] << std::endl;
+		exit(-1);
+	}
+
+    const unsigned int N = lines*samples;
+
+	delete[] image;
+	delete[] interleave;
+	delete[] waveUnit;
+	delete[] wavelength;
+
+	return 0;
+}
