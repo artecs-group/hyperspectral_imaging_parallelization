317b1df49fb5fa8d129a25f48522259738b4e06d
FRYoussef <f.r.youssef@hotmail.com>
2022-12-14 18:10:05 +0100

    Optimized OpenMP VCA

--- src/VCA/openmp/vca.cpp
+++ src/VCA/openmp/vca.cpp
@@ -85,41 +85,48 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
 	vslNewStream(&generator, VSL_BRNG_MT19937, seed);
 
     // get mean image
-	#pragma omp teams distribute
+	#pragma omp parallel for
 	for(int i = 0; i < bands; i++) {
-		#pragma omp single
+		double sum{0.0f};
+		#pragma omp simd reduction(+: sum)
 		for(int j = 0; j < N; j++)
-			mean[i] += image[i*N + j];
+			sum += image[i*N + j];
 
-		mean[i] /= N;
+		mean[i] = sum / N;
+	}
 
-		#pragma omp parallel for
+	#pragma omp parallel for simd
+	for(int i = 0; i < bands; i++) {
 		for(int j = 0; j < N; j++)
 			meanImage[i*N + j] = image[i*N + j] - mean[i];
 	}
 
 	cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, bands, bands, N, alpha, meanImage, N, meanImage, N, beta, svdMat, bands);
 
-	#pragma omp teams distribute parallel for
+	#pragma omp parallel for simd
 	for(int i = 0; i < bands * bands; i++) 
         svdMat[i] /= N;
 
     LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'S', 'S', bands, bands, svdMat, bands, D, U, bands, VT, bands, superb);
 
-	#pragma omp teams distribute parallel for collapse(2)
+	#pragma omp parallel for simd collapse(2)
 	for(int i = 0; i < bands; i++)
 		for(int j = 0; j < targetEndmembers; j++)
 			Ud[i*targetEndmembers + j] = VT[i*bands + j];
 
 	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, targetEndmembers, N, bands, alpha, Ud, targetEndmembers, meanImage, N, beta, x_p, targetEndmembers);
 
-	#pragma omp parallel for reduction(+:sum1, sum2, mult)
-	for(int i = 0; i < N*bands; i++) {
+	#pragma omp parallel for reduction(+: sum1)
+	for(int i = 0; i < N*bands; i++)
 		sum1 += image[i] * image[i];
-		if(i < N * targetEndmembers) 
-            sum2 += x_p[i] * x_p[i];
-		if(i < bands) 
-            mult += mean[i] * mean[i];
+
+	#pragma omp parallel for reduction(+: sum2)
+	for(int i = 0; i < N*targetEndmembers; i++)
+		sum2 += x_p[i] * x_p[i];
+
+	#pragma omp parallel for reduction(+: mult)
+	for(int i = 0; i < bands; i++) { 
+        mult += mean[i] * mean[i];
 	}
 
 	powery = sum1 / N;
@@ -130,21 +137,25 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
                     SNR;
 
 	if(SNR < SNR_th) {
-
 		#pragma omp teams distribute parallel for collapse(2)
 		for(int i = 0; i < bands; i++)
 			for(int j = 0; j < targetEndmembers; j++)
                 Ud[i*targetEndmembers + j] = (j < targetEndmembers-1) ? VT[i*bands + j] : 0;
 
-		sum1 = 0;
-		#pragma omp teams distribute parallel for
-		for(int i = 0; i < targetEndmembers; i++) {
-			for(int j = 0; j < N; j++) {
-				if(i == targetEndmembers-1) 
-                    x_p[i*N + j] = 0;
-				u[i] += x_p[i*N + j] * x_p[i*N + j];
-			}
+		#pragma omp parallel for simd
+		for(int j = 0; j < N; j++)
+        	x_p[(targetEndmembers-1)*N + j] = 0;
 
+		#pragma omp parallel for
+		for(int i = 0; i < targetEndmembers; i++) {
+			#pragma omp simd reduction(+: u[i])
+			for(int j = 0; j < N; j++)
+				u[i] += x_p[i*N + j] * x_p[i*N + j];
+		}
+
+		sum1 = 0;
+		#pragma omp parallel for reduction(max: sum1)
+		for(int i = 0; i < targetEndmembers; i++) {
 			if(sum1 < u[i]) 
                 sum1 = u[i];
 		}
@@ -153,7 +164,7 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
 
 		cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, bands, N, targetEndmembers, alpha, Ud, targetEndmembers, x_p, targetEndmembers, beta, Rp, bands);
 
-		#pragma omp teams distribute parallel for collapse(2)
+		#pragma omp teams distribute parallel for simd collapse(2)
 		for(int i = 0; i < bands; i++)
 			for(int j = 0; j < N; j++)
 				Rp[i*N + j] += mean[i];
@@ -167,13 +178,13 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
 
 		cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, bands, bands, N, alpha, image, N, image, N, beta, svdMat, bands);
 
-		#pragma omp teams distribute parallel for
+		#pragma omp teams distribute parallel for simd
 		for(int i = 0; i < bands*bands; i++)
             svdMat[i] /= N;
 
 		LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'S', 'S', bands, bands, svdMat, bands, D, U, bands, VT, bands, superb);
 
-		#pragma omp teams distribute parallel for collapse(2)
+		#pragma omp teams distribute parallel for simd collapse(2)
 		for(int i = 0; i < bands; i++)
 			for(int j = 0; j < targetEndmembers; j++)
 				Ud[i*targetEndmembers + j] = VT[i*bands + j];
@@ -181,25 +192,26 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
 		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, targetEndmembers, N, bands, alpha, Ud, targetEndmembers, image, N, beta, x_p, targetEndmembers);
 		cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, bands, N, targetEndmembers, alpha, Ud, targetEndmembers, x_p, targetEndmembers, beta, Rp, bands);
 
-		#pragma omp teams distribute
+		#pragma omp parallel for
 		for(int i = 0; i < targetEndmembers; i++) {
-
-			#pragma omp single
+			#pragma omp simd reduction(+: u[i])
 			for(int j = 0; j < N; j++)
 				u[i] += x_p[i*N + j];
+		}
 
-			#pragma omp parallel for
+		#pragma omp parallel for simd collapse(2)
+		for(int i = 0; i < targetEndmembers; i++) {
 			for(int j = 0; j < N; j++)
 				y[i*N + j] = x_p[i*N + j] * u[i];
 		}
 
-		#pragma omp teams distribute parallel for
+		#pragma omp parallel for
 		for(int i = 0; i < N; i++)
-			#pragma omp parallel for
+			#pragma omp simd reduction(+: sumxu[i])
 			for(int j = 0; j < targetEndmembers; j++)
 				sumxu[i] += y[j*N + i];
 
-		#pragma omp teams distribute parallel for collapse(2)
+		#pragma omp teams distribute parallel for simd collapse(2)
 		for(int i = 0; i < targetEndmembers; i++)
 			for(int j = 0; j < N; j++)
 				y[i*N + j] /= sumxu[j];
@@ -222,7 +234,7 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
 				1.0);
 		}
 
-        #pragma omp teams distribute parallel for
+        #pragma omp parallel for simd
 		for (size_t i = 0; i < targetEndmembers*targetEndmembers; i++)
 			A2[i] = A[i];
 
@@ -230,26 +242,16 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
         LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'S', 'S', targetEndmembers, targetEndmembers, A2, targetEndmembers, pinvS, pinvU, targetEndmembers, pinvVT, targetEndmembers, scarch_pinv);
 
         double maxi = std::numeric_limits<double>::min();
-		#pragma omp parallel
-		{
-			double pMax = std::numeric_limits<double>::min();
-			#pragma omp parallel for
-			for(int i = 0; i < targetEndmembers; i++)
-				if(pMax < pinvS[i]) 
-					pMax = pinvS[i];
-
-			#pragma omp critical
-			{
-				if(pMax > maxi)
-					maxi = pMax;
-			}
-		}
+		
+		#pragma omp prallel for reduction(max: maxi)
+		for(int i = 0; i < targetEndmembers; i++)
+			if(maxi < pinvS[i]) 
+				maxi = pinvS[i];
 
 		double tolerance = EPSILON * targetEndmembers * maxi;
-
         int rank = 0;
 
-		#pragma omp teams distribute parallel for reduction(+: rank)
+		#pragma omp parallel for reduction(+: rank)
         for (int i = 0; i < targetEndmembers; i++) {
             if (pinvS[i] > tolerance) {
                 rank++;
@@ -257,7 +259,7 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
             }
         }
 
-		#pragma omp teams distribute parallel for collapse(2)
+		#pragma omp teams for simd collapse(2)
         for (int i = 0; i < targetEndmembers; i++)
             for (int j = 0; j < targetEndmembers; j++) 
                 Utranstmp[i + j * targetEndmembers] = pinvS[i] * pinvU[j + i * targetEndmembers];
@@ -268,39 +270,39 @@ void OpenMP_VCA::_runOnCPU(float SNR, const double* image) {
 		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, targetEndmembers, targetEndmembers, targetEndmembers, alpha, A2, targetEndmembers, A, targetEndmembers, beta, aux, targetEndmembers);
 		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, targetEndmembers, alpha, targetEndmembers, alpha, aux, targetEndmembers, w, targetEndmembers, beta, f, targetEndmembers);
 
-	    sum1 = 0;
-
-		#pragma omp teams distribute parallel for reduction(+: sum1)
-	    for(int j = 0; j < targetEndmembers; j++) {
-	    	f[j] = w[j] - f[j];
-	    	sum1 += f[j] * f[j];
-	    }
-
-		#pragma omp teams distribute parallel for
+		#pragma omp parallel for simd
 	    for(int j = 0; j < targetEndmembers; j++)
-            f[j] /= sqrt(sum1);
+	    	f[j] = w[j] - f[j];
+
+	    sum1 = 0;
+		#pragma omp parallel for reduction(+: sum1)
+	    for(int j = 0; j < targetEndmembers; j++)
+	    	sum1 += f[j] * f[j];
+
+		const double sqrt_s1 = std::sqrt(sum1);
+		#pragma omp parallel for simd
+	    for(int j = 0; j < targetEndmembers; j++)
+            f[j] /= sqrt_s1;
 
 		cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, alpha, N, targetEndmembers, alpha, f, alpha, y, N, beta, sumxu, alpha);
 
-	    sum2 = 0;
-		#pragma omp teams distribute parallel for
+		#pragma omp parallel for simd
 	    for(int j = 0; j < N; j++) {
 	    	if(sumxu[j] < 0) 
                 sumxu[j] *= -1;
 	    }
 
+		#pragma omp parallel for reduction(max: index[i])
 	    for(int j = 0; j < N; j++) {
-	    	if(sum2 < sumxu[j]) {
-	    		sum2 = sumxu[j];
+	    	if(sumxu[j] > sumxu[index[i]])
 	    		index[i] = j;
-	    	}
 	    }
 
-		#pragma omp teams distribute parallel for
+		#pragma omp parallel for simd
 	    for(int j = 0; j < targetEndmembers; j++)
 	    	A[j*targetEndmembers + i] = y[j*N + index[i]];
 
-		#pragma omp teams distribute parallel for
+		#pragma omp parallel for simd
 	    for(int j = 0; j < bands; j++)
 	    	endmembers[j*targetEndmembers + i] = Rp[j + bands * index[i]];
 	}
@@ -366,15 +368,18 @@ void OpenMP_VCA::_runOnGPU(float SNR, const double* image) {
 	device(default_dev)
 	{
 		// get mean image
-		#pragma omp target teams distribute
+		#pragma omp target distribute teams parallel for
 		for(int i = 0; i < bands; i++) {
-			#pragma omp single
+			double sum{0.0f};
+			#pragma omp simd reduction(+: sum)
 			for(int j = 0; j < N; j++)
-				mean[i] += image[i*N + j];
+				sum += image[i*N + j];
 
-			mean[i] /= N;
+			mean[i] = sum / N;
+		}
 
-			#pragma omp parallel for
+		#pragma omp target distribute teams parallel for simd
+		for(int i = 0; i < bands; i++) {
 			for(int j = 0; j < N; j++)
 				meanImage[i*N + j] = image[i*N + j] - mean[i];
 		}
@@ -403,15 +408,23 @@ void OpenMP_VCA::_runOnGPU(float SNR, const double* image) {
 			cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, targetEndmembers, N, bands, alpha, Ud, targetEndmembers, meanImage, N, beta, x_p, targetEndmembers);
 		}
 
-		#pragma omp target teams distribute map(sum1, sum2, mult) reduction(+: sum1, sum2, mult)
-		for(int i = 0; i < N*bands; i++) {
+		#pragma omp target teams distribute parallel for reduction(+: sum1) map(sum1)
+		for(int i = 0; i < N*bands; i++){
 			sum1 += image[i] * image[i];
-			if(i < N * targetEndmembers) 
-				sum2 += x_p[i] * x_p[i];
-			if(i < bands) 
-				mult += mean[i] * mean[i];
 		}
-		#pragma omp target update from(sum1, sum2, mult)
+		#pragma omp target update from(sum1)
+
+		#pragma omp target teams distribute parallel for reduction(+: sum2) map(sum2)
+		for(int i = 0; i < N*targetEndmembers; i++){
+			sum2 += x_p[i] * x_p[i];
+		}
+		#pragma omp target update from(sum2)
+
+		#pragma omp target teams distribute parallel for reduction(+: mult) map(mult)
+		for(int i = 0; i < bands; i++) { 
+			mult += mean[i] * mean[i];
+		}
+		#pragma omp target update from(mult)
 
 		powery = sum1 / N;
 		powerx = sum2 / N + mult;
@@ -427,19 +440,22 @@ void OpenMP_VCA::_runOnGPU(float SNR, const double* image) {
 				for(int j = 0; j < targetEndmembers; j++)
 					Ud[i*targetEndmembers + j] = (j < targetEndmembers-1) ? VT[i*bands + j] : 0;
 
+			#pragma omp target teams distribute parallel for simd
+			for(int j = 0; j < N; j++)
+				x_p[(targetEndmembers-1)*N + j] = 0;
+
+			#pragma omp target teams distribute parallel for
+			for(int i = 0; i < targetEndmembers; i++) {
+				#pragma omp simd reduction(+: u[i])
+				for(int j = 0; j < N; j++)
+					u[i] += x_p[i*N + j] * x_p[i*N + j];
+			}
+
 			#pragma omp target map(sum1)
 			{sum1 = 0;}
 
-			#pragma omp target teams distribute map(sum1)
+			#pragma omp target teams distribute parallel for reduction(max: sum1) map(sum1)
 			for(int i = 0; i < targetEndmembers; i++) {
-
-				#pragma omp parallel for
-				for(int j = 0; j < N; j++) {
-					if(i == targetEndmembers-1) 
-						x_p[i*N + j] = 0;
-					u[i] += x_p[i*N + j] * x_p[i*N + j];
-				}
-
 				if(sum1 < u[i]) 
 					sum1 = u[i];
 			}
@@ -493,27 +509,27 @@ void OpenMP_VCA::_runOnGPU(float SNR, const double* image) {
 				cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, bands, N, targetEndmembers, alpha, Ud, targetEndmembers, x_p, targetEndmembers, beta, Rp, bands);
 			}
 
-			#pragma omp target teams distribute
+			#pragma omp target teams distribute parallel for
 			for(int i = 0; i < targetEndmembers; i++) {
-
-				#pragma omp single
+				#pragma omp simd reduction(+: u[i])
 				for(int j = 0; j < N; j++)
 					u[i] += x_p[i*N + j];
+			}
 
-				#pragma omp parallel for
+			#pragma omp target teams distribute parallel for simd collapse(2)
+			for(int i = 0; i < targetEndmembers; i++) {
 				for(int j = 0; j < N; j++)
 					y[i*N + j] = x_p[i*N + j] * u[i];
 			}
 
 			#pragma omp target teams distribute parallel for
 			for(int i = 0; i < N; i++)
-				#pragma omp parallel for
+				#pragma omp reduction(+: sumxu[i])
 				for(int j = 0; j < targetEndmembers; j++)
 					sumxu[i] += y[j*N + i];
 
-			#pragma omp target teams distribute
+			#pragma omp target teams distribute parallel for collapse(2)
 			for(int i = 0; i < targetEndmembers; i++)
-				#pragma omp single
 				for(int j = 0; j < N; j++)
 					y[i*N + j] /= sumxu[j];
 		}
@@ -547,7 +563,8 @@ void OpenMP_VCA::_runOnGPU(float SNR, const double* image) {
 
 			#pragma omp target parallel for reduction(max: maxi)
 			for(int i = 0; i < targetEndmembers; i++)
-				maxi = std::max(pinvS[i], maxi);
+				if(maxi < pinvS[i]) 
+					maxi = pinvS[i];
 			#pragma omp target update from(maxi)
 
 			double tolerance = EPSILON * targetEndmembers * maxi;
@@ -606,15 +623,10 @@ void OpenMP_VCA::_runOnGPU(float SNR, const double* image) {
 					sumxu[j] *= -1;
 			}
 
-			#pragma omp target map(sum2)
-			{sum2 = std::numeric_limits<double>::min();}
-
-			#pragma omp target
+			#pragma omp target teams distribute parallel for reduction(max: index[i])
 			for(int j = 0; j < N; j++) {
-				if(sum2 < sumxu[j]) {
-					sum2 = sumxu[j];
+				if(sumxu[j] > sumxu[index[i]])
 					index[i] = j;
-				}
 			}
 
 			#pragma omp target teams distribute parallel for
