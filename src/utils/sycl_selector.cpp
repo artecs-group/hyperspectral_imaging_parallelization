b120efe60b9a111750d8932f2126f5a68349ac19
FRYoussef <f.r.youssef@hotmail.com>
2023-01-17 11:53:24 +0100

    Changed the way to have selectors to align to SYCL2020

--- src/utils/sycl_selector.cpp
+++ src/utils/sycl_selector.cpp
@@ -7,16 +7,31 @@ sycl::queue get_queue(){
         return *dev_queue;
 
 #if defined(INTEL_GPU)
-	IntelGPUSelector selector{};
+    auto intel_gpu_selector = [](const sycl::device &Device) {
+        const std::string vendor = Device.get_info<sycl::info::device::vendor>();
+
+        if (Device.is_gpu() && (vendor.find("Intel(R) Corporation") != std::string::npos))
+            return 1;
+
+        return -1;
+    };
+    dev_queue = new sycl::queue{intel_gpu_selector};
 #elif defined(NVIDIA_GPU)
-	NvidiaGPUSelector selector{};
+    auto nvidia_gpu_selector = [](const sycl::device &Device) {
+        const std::string DriverVersion = Device.get_info<sycl::info::device::driver_version>();
+
+        if (Device.is_gpu() && (DriverVersion.find("CUDA") != std::string::npos))
+            return 1;
+
+        return -1;
+    };
+    dev_queue = new sycl::queue{nvidia_gpu_selector};
 #elif defined(CPU)
-	sycl::cpu_selector selector{};
+    dev_queue = new sycl::queue{sycl::cpu_selector_v};
 #else
-	default_selector selector{};
+    dev_queue = new sycl::queue{sycl::default_selector_v};
 #endif
 
-    dev_queue = new sycl::queue{selector};
     std::cout << std::endl << "Running on: "
             << dev_queue->get_device().get_info<sycl::info::device::name>()
             << std::endl << std::endl;
