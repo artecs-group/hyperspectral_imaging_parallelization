f78e4758d25c1e059fa2aaae922c5901900cd080
FRYoussef <f.r.youssef@hotmail.com>
2023-02-02 17:29:26 +0100

    moved from row major to col major in vd

--- sequential/VD/vd.cpp
+++ sequential/VD/vd.cpp
@@ -7,20 +7,20 @@
 
 #include "vd.hpp"
 
-SequentialVD::SequentialVD(int _lines, int _samples, int _bands){
-    lines      = _lines;
-    samples    = _samples;
-    bands      = _bands;
-    meanSpect  = new double[bands];
-    Cov		   = new double[bands * bands];
-    Corr	   = new double[bands * bands];
-    CovEigVal  = new double[bands];
+SequentialVD::SequentialVD(int _lines, int _samples, int _bands) {
+    lines = _lines;
+    samples = _samples;
+    bands = _bands;
+    meanSpect = new double[bands];
+    Cov = new double[bands * bands];
+    Corr = new double[bands * bands];
+    CovEigVal = new double[bands];
     CorrEigVal = new double[bands];
-    U		   = new double[bands * bands];
-    VT	       = new double[bands * bands];
-    count      = new unsigned int[FPS];
+    U = new double[bands * bands];
+    VT = new double[bands * bands];
+    count = new unsigned int[FPS];
     estimation = new double[FPS];
-    meanImage  = new double [lines * samples * bands];
+    meanImage = new double[lines * samples * bands];
 
     // table where find the estimation by FPS
     estimation[0] = 0.906193802436823;
@@ -37,16 +37,16 @@ SequentialVD::~SequentialVD() {
 
 
 void SequentialVD::clearMemory() {
-    if(meanSpect != nullptr) {delete[] meanSpect; meanSpect = nullptr; }
-    if(Cov != nullptr) {delete[] Cov; Cov = nullptr; }
-    if(Corr != nullptr) {delete[] Corr; Corr = nullptr; }
-    if(CovEigVal != nullptr) {delete[] CovEigVal; CovEigVal = nullptr; }
-    if(CorrEigVal != nullptr) {delete[] CorrEigVal; CorrEigVal = nullptr; }
-    if(U != nullptr) {delete[] U; U = nullptr; }
-    if(VT != nullptr) {delete[] VT; VT = nullptr; }
-    if(count != nullptr) {delete[] count; count = nullptr; }
-    if(estimation != nullptr) {delete[] estimation; estimation = nullptr; }
-    if(meanImage != nullptr) {delete[] meanImage; meanImage = nullptr; }
+    if (meanSpect != nullptr) { delete[] meanSpect; meanSpect = nullptr; }
+    if (Cov != nullptr) { delete[] Cov; Cov = nullptr; }
+    if (Corr != nullptr) { delete[] Corr; Corr = nullptr; }
+    if (CovEigVal != nullptr) { delete[] CovEigVal; CovEigVal = nullptr; }
+    if (CorrEigVal != nullptr) { delete[] CorrEigVal; CorrEigVal = nullptr; }
+    if (U != nullptr) { delete[] U; U = nullptr; }
+    if (VT != nullptr) { delete[] VT; VT = nullptr; }
+    if (count != nullptr) { delete[] count; count = nullptr; }
+    if (estimation != nullptr) { delete[] estimation; estimation = nullptr; }
+    if (meanImage != nullptr) { delete[] meanImage; meanImage = nullptr; }
 }
 
 
@@ -54,48 +54,48 @@ void SequentialVD::run(const int approxVal, const double* image) {
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
     float tVd{0.f};
     double mean{0.f}, TaoTest{0.f}, sigmaTest{0.f}, sigmaSquareTest{0.f};
-    const unsigned int N{lines*samples};
-    const double alpha{(double) 1/N}, beta{0};
-    double superb[bands-1];
+    const unsigned int N{lines * samples};
+    const double alpha{(double)1 / N}, beta{0};
+    double superb[bands - 1];
 
     start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < bands; i++) {
-		mean = 0.f;
-        mean = std::accumulate(image + (i*N), image + (i*N + N), 0);
-		mean /= N;
+    for (int i = 0; i < bands; i++) {
+        mean = 0.f;
+        mean = std::accumulate(image + (i * N), image + (i * N + N), 0);
+        mean /= N;
         meanSpect[i] = mean;
 
-        for(int j = 0; j < N; j++)
-			meanImage[i*N + j] = image[i*N + j] - mean;
-	}
+        for (int j = 0; j < N; j++)
+            meanImage[i * N + j] = image[i * N + j] - mean;
+    }
 
-    cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, bands, bands, N, alpha, meanImage, N, meanImage, N, beta, Cov, bands);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, bands, bands, N, alpha, meanImage, N, meanImage, N, beta, Cov, bands);
 
-	//correlation
-    for(int j = 0; j < bands; j++)
-        for(int i = 0; i < bands; i++)
-        	Corr[i*bands + j] = Cov[i*bands + j]+(meanSpect[i] * meanSpect[j]);
+    //correlation
+    for (int j = 0; j < bands; j++)
+        for (int i = 0; i < bands; i++)
+            Corr[i * bands + j] = Cov[i * bands + j] + (meanSpect[i] * meanSpect[j]);
 
-	//SVD
-    LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'N', 'N', bands, bands, Cov, bands, CovEigVal, U, bands, VT, bands, superb);
-    LAPACKE_dgesvd(LAPACK_COL_MAJOR, 'N', 'N', bands, bands, Corr, bands, CorrEigVal, U, bands, VT, bands, superb);
+    //SVD
+    LAPACKE_dgesvd(LAPACK_ROW_MAJOR, 'N', 'N', bands, bands, Cov, bands, CovEigVal, U, bands, VT, bands, superb);
+    LAPACKE_dgesvd(LAPACK_ROW_MAJOR, 'N', 'N', bands, bands, Corr, bands, CorrEigVal, U, bands, VT, bands, superb);
 
     //estimation
-    std::fill(count, count+FPS, 0);
+    std::fill(count, count + FPS, 0);
 
-    for(int i = 0; i < bands; i++) {
-    	sigmaSquareTest = (CovEigVal[i]*CovEigVal[i] + CorrEigVal[i]*CorrEigVal[i]) * 2 / samples / lines;
-    	sigmaTest = sqrt(sigmaSquareTest);
+    for (int i = 0; i < bands; i++) {
+        sigmaSquareTest = (CovEigVal[i] * CovEigVal[i] + CorrEigVal[i] * CorrEigVal[i]) * 2 / samples / lines;
+        sigmaTest = sqrt(sigmaSquareTest);
 
-    	for(int j = 1; j <= FPS; j++) {
-            TaoTest = M_SQRT2 * sigmaTest * estimation[j-1];
+        for (int j = 1; j <= FPS; j++) {
+            TaoTest = M_SQRT2 * sigmaTest * estimation[j - 1];
 
-            if((CorrEigVal[i] - CovEigVal[i]) > TaoTest)
-                count[j-1]++;
+            if ((CorrEigVal[i] - CovEigVal[i]) > TaoTest)
+                count[j - 1]++;
         }
     }
 
-    endmembers = count[approxVal-1];
+    endmembers = count[approxVal - 1];
     end = std::chrono::high_resolution_clock::now();
     tVd += std::chrono::duration_cast<std::chrono::duration<float>>(end - start).count();
 #if defined(DEBUG)
